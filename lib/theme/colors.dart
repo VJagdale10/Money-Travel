a9e140044708b3cdd814592182826be3aec690e6
VJagdale10 <vaishnavi.cs19@bmsce.ac.in>
2022-01-01 19:15:55 +0530

    First commit

---
+++ lib/theme/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const Color primary = Color(0xFF2C3137);
+const Color secondary = Color(0xFF2C3137);
+const Color black = Color(0xFF000000);
+const Color white = Color(0xFFFFFFFF);
+const Color grey = Color(0xFFEDE6E3);
+const Color red = Color(0xfff05f42);
+const Color green = Color(0xffc3cb75);
+const Color blue = Color(0xff147ba3);
+const Color errorColor = red;
+
+const Color succses = green;
